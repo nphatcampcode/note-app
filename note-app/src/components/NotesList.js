12f0a108b0e96381b34d99269809c56dd2bb16f9
Phat tran <ngocphxt2018@gmail.com>
2024-01-20 18:21:18 +0700

    A fix I believe, not like I tested or anything

--- note-app/src/components/NotesList.js
+++ note-app/src/components/NotesList.js
@@ -1,16 +1,15 @@
-import Note from './Note';
-import AddNote from './AddNote';
+import Note from "./Note";
+import AddNote from "./AddNote";
 
-const NotesList = ({notes}) => {
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
     return (
         <div className="notes-list">
-            {notes.map((note)=> (
-            <Note id={note.id} text={note.text} date={note.date}/> 
+            {notes.map((note) => (
+                <Note id={note.id} text={note.text} date={note.date} handleDeleteNote={handleDeleteNote} />
             ))}
-            <AddNote />
-         
+            <AddNote handleAddNote={handleAddNote} />
         </div>
-    )
-}
+    );
+};
 
 export default NotesList;
